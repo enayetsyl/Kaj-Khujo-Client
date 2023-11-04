e50d8c7d436da2bdefb21bb16ee7c4664de41325
Enayet <enayetflweb@gamil.com>
2023-11-04 22:22:35 +0600

    firebase setup, authProvider done, env file done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,66 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from "../Firebase/firebase.config";
 
-const AuthProvider = () => {
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+
+const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null);
+  const [userName, setUserName] = useState(null);
+  const [userPhoto, setUserPhoto] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true)
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const signInUser = (email, password) => {
+    setLoading(true)
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const googleSignIn = () => {
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider)
+  }
+
+  const logOut = () => {
+    setLoading(true)
+    return signOut(auth)
+  }
+
+  useEffect(() => {
+    const subscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false)
+    });
+    return () => {
+      subscribe();
+    }
+  },[])
+
+  const authInfo = {
+    user, 
+    userName,
+    setUserName,
+    userPhoto,
+    setUserPhoto,
+    loading,
+    createUser,
+    signInUser,
+    googleSignIn, 
+    logOut
+
+  }
+
   return (
-    <div>
-      
-    </div>
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
