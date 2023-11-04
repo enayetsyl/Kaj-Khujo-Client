c2a20af46a24f6e9364e68b5e08b3527738d45c6
Enayet <enayetflweb@gamil.com>
2023-11-04 20:41:48 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Component/Navbar";
+import Footer from "../Component/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>      
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
